268bfe1f24593a7cb197317a85342d1c9264182b
Ferreyra Enzo <ferreyraenzo26@hotmail.com>
2024-04-17 15:58:23 -0300

    Cambios en el menu de registrar venta

--- AccesoBD/Service/ServicesImpl/SaleCalculatorService.cs
+++ AccesoBD/Service/ServicesImpl/SaleCalculatorService.cs
@@ -11,27 +11,44 @@ namespace SistemaGestionVentas.Service
 
     public class SaleCalculatorService : ISaleCalculatorService
     {
-        public (decimal subtotal, decimal totalDiscount, decimal totalPay) 
+        public (decimal subtotal, decimal totalDiscount, decimal totalPay)
             CalculateSaleDetails(IList<Product> products)
         {
+            Console.WriteLine("¿Desea ver el detalle de los productos? (Si/No)");
+            string response = Console.ReadLine();
+
+            if (response.ToLower() == "si")
+            {
+                // Mostrar detalle de los productos
+                foreach (var product in products)
+                {
+                    Console.WriteLine($"Producto: {product.Name}");
+                    Console.WriteLine($"Precio: {product.Price:C}");
+                    Console.WriteLine($"Descuento:({product.Discount:F2}%)");
+
+                    Console.WriteLine("-----------------------------");
+                }
+            }
+            else
+            {
+                Console.WriteLine("-----------------------------");
+            }
 
             // Calcular subtotal, descuento total y total de la venta basado en productos seleccionados
             decimal subtotal = CalculateSubtotal(products);
             decimal totalDiscount = CalculateTotalDiscount(products);
             decimal taxes = Constantes.Taxes;
             decimal totalPay = (subtotal - totalDiscount) * taxes;
-            
+
             // Mostrar importe total, subtotal y descuento
             Console.WriteLine($"Subtotal: {subtotal:C}");
-            Console.WriteLine($"Descuento: {totalDiscount:C}");
+            Console.WriteLine($"Descuento:({totalDiscount:F2}%)");
             Console.WriteLine($"Importe Total: {totalPay:C}");
 
             return (subtotal, totalDiscount, totalPay);
-
- 
         }
 
-        private decimal CalculateSubtotal(IList<Product> products)
+    private decimal CalculateSubtotal(IList<Product> products)
         {
             return products.Sum(p => p.Price);
         }
